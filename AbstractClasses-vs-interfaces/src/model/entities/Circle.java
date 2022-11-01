db958bed482ac9e0d5e7f8aec67ae0a2b8adf960
larramendi <larramendi.dev@gmail.com>
2022-10-31 21:22:35 -0300

    Abstract class only

---
+++ AbstractClasses-vs-interfaces/src/model/entities/Circle.java
@@ -0,0 +1,25 @@
+package model.entities;
+
+import model.enums.Color;
+
+public class Circle extends Shape {
+
+    private Double radius;
+
+    public Circle(Color color, Double radius) {
+        super(color);
+        this.radius = radius;
+    }
+
+    public Double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(Double radius) {
+        this.radius = radius;
+    }
+
+    public double area() {
+        return 3.14 * radius * radius;
+    }
+}
